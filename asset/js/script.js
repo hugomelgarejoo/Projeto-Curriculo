96ea030be3e3addfa69f9351232f392b618fa741
hugomelgarejoo <hugomelgarejo987@gmail.com>
2023-10-02 22:00:32 -0300

    em manutenção
    falta atualizacoes de acordo com o aprendizado

---
+++ asset/js/script.js
@@ -0,0 +1,30 @@
+
+// Função para Efeito Digitando.
+
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra,i)=> {
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+}
+
+// Função para Menu
+
+function menu(){
+    const ativaMenu = document.querySelector('.icone');
+    const navMenu = document.querySelector('header .navbar')
+    
+    ativaMenu.addEventListener('click', ()=> {
+        navMenu.classList.toggle('ativado')
+        
+    })
+}
+
